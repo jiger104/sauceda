5fe4d6df4a5d973f2f6e99b1cfc162211c411515
jigarpatel <jigarpatel@scalefactor.com>
2018-07-08 13:08:24 -0500

    tsheets api call

--- tsheets.py
+++ tsheets.py
@@ -7,7 +7,10 @@ payload = {"data": {"start_date": "2017-03-12","end_date": "2017-03-18"}}
 headers = {'Authorization':'Bearer S.1__eca36b67bc526e97b6dc1e67caabbf1e896e887b'}
 myResponse = requests.post(url, headers=headers, json=payload)
 
-if(myResponse.ok):
+with open('out.json', 'w') as f:
+   f.write(myResponse.text)
+
+if (myResponse.ok):
     print("Success")
 else:
     print("Error")
