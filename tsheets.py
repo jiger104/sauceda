315e3a75b4234b4b15764681067f45809df7ea0a
jigar <jigarpatel@scalefactor.com>
2018-07-08 23:33:08 -0500

    updated date variables

--- tsheets.py
+++ tsheets.py
@@ -4,19 +4,15 @@ import json
 import datetime
 
 url = "https://rest.tsheets.com/api/v1/reports/payroll_by_jobcode"
-s_date = datetime.datetime.today().strftime('%Y-%m-%d')
-e_date = datetime.datetime.now
-payload = {"data": {"start_date": "2017-03-12","end_date": "2017-03-18"}}
+date = datetime.datetime.today().strftime('%Y-%m-%d')
+payload = {"data": {"start_date": "{}".format(date),"end_date": "{}".format(date)}}
 headers = {'Authorization':'Bearer S.1__eca36b67bc526e97b6dc1e67caabbf1e896e887b'}
 myResponse = requests.post(url, headers=headers, json=payload)
 
-print(s_date)
-
-# with open('out.json', 'w') as f:
-#    f.write(myResponse.text)
+with open('out.json', 'w') as f:
+    f.write(myResponse.text)
 
 if (myResponse.ok):
-    print("Success")
-
+    print("T-Sheets API Success")
 else:
-    print("Error")
+    print("T-Sheets API Error")
